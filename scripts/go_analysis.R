9e1fbb5fd9536a00098ded079b1ddbd37d54f2f0
sejalgupta0304 <sejalg454@gmail.com>
2026-06-08 15:34:57 +0530

    Create go_analysis.R

---
+++ scripts/go_analysis.R
@@ -0,0 +1,26 @@
+library(clusterProfiler)
+library(org.Mm.eg.db)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+deg <- read.csv(args[1])
+
+sig <- subset(
+  deg,
+  padj < 0.05 &
+  abs(log2FoldChange) > 1
+)
+
+genes <- rownames(sig)
+
+go <- enrichGO(
+  gene = genes,
+  OrgDb = org.Mm.eg.db,
+  keyType = "SYMBOL",
+  ont = "BP"
+)
+
+write.csv(
+  as.data.frame(go),
+  "GO_results.csv"
+)
